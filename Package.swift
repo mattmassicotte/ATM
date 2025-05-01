b01960c2e9b8757b7ef15622849a225ff0e41c42
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-05-01 17:33:28 -0400

    platform version minimums

--- Package.swift
+++ Package.swift
@@ -6,6 +6,11 @@ let package = Package(
 	name: "ATM",
 	platforms: [
 		.macOS(.v10_15),
+		.macCatalyst(.v13),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.watchOS(.v6),
+		.visionOS(.v1),
 	],
 	products: [
 		.library(name: "ATM", targets: ["ATM"]),
