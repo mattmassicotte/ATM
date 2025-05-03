2f850c8d21f75c1a0692fad8c68a5b18737c88d8
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-05-03 07:44:46 -0400

    Add some commentdocs for backing stores

--- Sources/ATM/BackingStore.swift
+++ Sources/ATM/BackingStore.swift
@@ -1,3 +1,4 @@
+/// A synchronous cache backing store.
 public protocol BackingStore<Key, Value> {
 	associatedtype Key: Hashable
 	associatedtype Value
@@ -6,6 +7,7 @@ public protocol BackingStore<Key, Value> {
 	mutating func write(_ key: Key, _ value: Value?)
 }
 
+/// An asynchronous cache backing store.
 public protocol AsyncBackingStore<Key, Value> {
 	associatedtype Key: Hashable
 	associatedtype Value
@@ -14,7 +16,10 @@ public protocol AsyncBackingStore<Key, Value> {
 	mutating func write(_ key: Key, _ value: Value?, actor: isolated (any Actor)?) async
 }
 
+/// Defines how data written to the cache is propagated to its backing stores.
 public enum WritePolicy {
+	/// Values are written to the cache and subsequent level immediately.
 	case writeThrough
+	/// Values are only written to the cache immediately.
 	case writeBack
 }
